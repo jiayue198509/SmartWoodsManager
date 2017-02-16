748820869e383193742b44b43bbc98c0e7112c23
jiaerdong <jiaerdong@jiaerdongdeMacBook-Pro.local>
2017-02-16 15:40:42 +0800

    创建工程

---
+++ SmartWoodsManager/SmartWoodsManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartWoodsManager
+//
+//  Created by jiaerdong on 2017/2/16.
+//  Copyright © 2017年 springwoods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
